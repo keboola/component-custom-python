510dfb7c91ec45839c3436880d3f346a64621e4b
soustruh <martin@struzsky.cz>
2025-03-04 15:58:46 +0100

    uv for dependency management 💜

--- Dockerfile
+++ Dockerfile
@@ -1,37 +1,21 @@
 FROM quay.io/keboola/docker-custom-python:latest
-ENV PYTHONIOENCODING utf-8
 
+# install gcc to be able to build packages - e.g. required by regex, dateparser, also required for pandas
+# RUN apt-get update && apt-get install -y build-essential
 
+RUN pip install uv
 
-# Create directory for user packages
-# This directory is usually created automatically by pip
-# ... but if it doesn't exist when you run the script
-# ... then the modules installed during the transformation are not loaded automatically!
-# ... because the loader thinks that this directory does not exist (it did not exist at the start of the script)
-# Eg. mkdir -p /var/www/.local/lib/python3.8/site-packages
-RUN . "$VIRTUAL_ENV/bin/activate" && mkdir -p $(su www-data -s /bin/bash -c "python -c 'import site; print(site.USER_SITE)'")
+COPY requirements.txt /code/requirements.txt
+COPY requirements-tests.txt /code/requirements-tests.txt
 
-# Make home directory writable
-RUN chown -R www-data:www-data /var/www
+RUN uv pip sync --system /code/requirements.txt /code/requirements-tests.txt
 
 COPY /src /code/src/
 COPY /tests /code/tests/
 COPY /scripts /code/scripts/
-COPY requirements.txt /code/requirements.txt
-COPY requirements-tests.txt /code/requirements-tests.txt
 COPY flake8.cfg /code/flake8.cfg
 COPY deploy.sh /code/deploy.sh
 
-# install gcc to be able to build packages - e.g. required by regex, dateparser, also required for pandas
-RUN apt-get update && apt-get install -y build-essential
-
-RUN . "$VIRTUAL_ENV/bin/activate" && pip freeze
-
-RUN . "$VIRTUAL_ENV/bin/activate" && pip install -r /code/requirements-tests.txt
-
-RUN . "$VIRTUAL_ENV/bin/activate" && pip install -r /code/requirements.txt
-
 WORKDIR /code/
 
-RUN . "$VIRTUAL_ENV/bin/activate" && pip freeze
-CMD . "$VIRTUAL_ENV/bin/activate" && python -u /code/src/component.py
+CMD uv run /code/src/component.py
--- src/component.py
+++ src/component.py
@@ -2,19 +2,22 @@
 Template Component main class.
 
 """
+
 import json
 import logging
 import os
 import runpy
+import subprocess
 import sys
+import traceback
 from traceback import TracebackException
 
 from keboola.component.base import ComponentBase
 from keboola.component.exceptions import UserException
 
 # configuration variables
-KEY_API_TOKEN = '#api_token'
-KEY_PRINT_HELLO = 'print_hello'
+KEY_API_TOKEN = "#api_token"
+KEY_PRINT_HELLO = "print_hello"
 
 # list of mandatory parameters => if some is missing,
 # component will fail with readable message on initialization.
@@ -24,13 +27,13 @@ REQUIRED_IMAGE_PARS = []
 
 class Component(ComponentBase):
     """
-        Extends base class for general Python components. Initializes the CommonInterface
-        and performs configuration validation.
+    Extends base class for general Python components. Initializes the CommonInterface
+    and performs configuration validation.
 
-        For easier debugging the data folder is picked up by default from `../data` path,
-        relative to working directory.
+    For easier debugging the data folder is picked up by default from `../data` path,
+    relative to working directory.
 
-        If `debug` parameter is present in the `config.json`, the default logger is set to verbose DEBUG mode.
+    If `debug` parameter is present in the `config.json`, the default logger is set to verbose DEBUG mode.
     """
 
     def __init__(self):
@@ -41,41 +44,39 @@ class Component(ComponentBase):
 
         self._set_init_logging_handler()
         logging.info(sys.executable)
-        script_path = os.path.join(self.data_folder_path, 'script.py')
+        script_path = os.path.join(self.data_folder_path, "script.py")
         self.prepare_script_file(script_path)
 
         self._merge_user_parameters()
 
         # install packages
-        self.install_packages(parameters.get('packages', []))
+        self.install_packages(parameters.get("packages", []))
 
         self.execute_script_file(script_path)
 
     def prepare_script_file(self, destination_path: str):
-        script = self.configuration.parameters['code']
-        with open(destination_path, 'w+') as file:
+        script = self.configuration.parameters["code"]
+        with open(destination_path, "w+") as file:
             file.write(script)
 
     def execute_script_file(self, file_path):
-        import traceback
-
         # Change current working directory so that relative paths work
         os.chdir(self.data_folder_path)
         sys.path.append(self.data_folder_path)
 
         try:
-            with open(file_path, 'r') as file:
+            with open(file_path, "r") as file:
                 script = file.read()
             logging.info('Execute script "%s"' % (self.script_excerpt(script)))
             runpy.run_path(file_path)
-            logging.info('Script finished')
+            logging.info("Script finished")
         except Exception as err:
             _, _, tb = sys.exc_info()
             stack_len = len(traceback.extract_tb(tb)[4:])
             stack_trace_records = self._get_stack_trace_records(*sys.exc_info(), -stack_len, chain=True)
             stack_cropped = "\n".join(stack_trace_records)
 
-            raise UserException(f'Script failed. {err}. Detail: {stack_cropped}') from err
+            raise UserException(f"Script failed. {err}. Detail: {stack_cropped}") from err
 
     @staticmethod
     def _get_stack_trace_records(etype, value, tb, limit=None, chain=True):
@@ -87,42 +88,35 @@ class Component(ComponentBase):
     @staticmethod
     def script_excerpt(script):
         if len(script) > 1000:
-            return script[0: 500] + '\n...\n' + script[-500]
+            return script[0:500] + "\n...\n" + script[-500]
         else:
             return script
 
     @staticmethod
     def install_packages(packages):
-        import subprocess
-        import sys
         for package in packages:
             args = [
-                sys.executable,
-                '-m', 'pip', 'install',
-                '--disable-pip-version-check',
-                '--no-cache-dir',
-                '--no-warn-script-location',  # ignore error: installed in '/var/www/.local/bin' which is not on PATH.
-                '--force-reinstall',
-                package
+                "uv",
+                "pip",
+                "install",
+                package,
             ]
-            process = subprocess.Popen(args,
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE)
+            process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, stderr = process.communicate()
-            logging.info(f'Installing package: {package}. Full log in detail.', extra={'full_message': stdout})
+            logging.info(f"Installing package: {package}. Full log in detail.", extra={"full_message": stdout})
             process.poll()
             if process.poll() != 0:
-                raise UserException(f'Failed to install package:  {package}. Log in event detail.', stderr)
+                raise UserException(f"Failed to install package: {package}. Log in event detail.", stderr)
             elif stderr:
                 logging.warning(stderr)
 
     def _set_init_logging_handler(self):
         for h in logging.getLogger().handlers:
-            h.setFormatter(logging.Formatter('[Non-script message]: %(message)s'))
+            h.setFormatter(logging.Formatter("[Non-script message]: %(message)s"))
 
     def _merge_user_parameters(self):
         """
-        INPLACE Merges user paramters into config.json->parameters property. Rebuilds the physical config.json file
+        INPLACE Merges user parameters into config.json->parameters property. Rebuilds the physical config.json file
         Returns:
 
         """
@@ -130,8 +124,8 @@ class Component(ComponentBase):
         config_data = self.configuration.config_data.copy()
 
         # build config data and overwrite for the user script
-        config_data['parameters'] = self.configuration.parameters.get('user_properties', {})
-        with open(os.path.join(self.data_folder_path, 'config.json'), 'w+') as inp:
+        config_data["parameters"] = self.configuration.parameters.get("user_properties", {})
+        with open(os.path.join(self.data_folder_path, "config.json"), "w+") as inp:
             json.dump(config_data, inp)
 
 
@@ -144,7 +138,7 @@ if __name__ == "__main__":
         # this triggers the run method by default and is controlled by the configuration.action parameter
         comp.execute_action()
     except UserException as exc:
-        detail = ''
+        detail = ""
         if len(exc.args) > 1:
             detail = exc.args[1]
         logging.exception(exc, extra={"full_message": detail})
